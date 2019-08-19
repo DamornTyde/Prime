91480573c2f24b6960e43198b9ea295a4755bd65
Ronald van Zon <34279265+DamornTyde@users.noreply.github.com>
2019-08-19 21:55:54 +0200

    Update functions.js

--- functions.js
+++ functions.js
@@ -7,25 +7,29 @@ $(document).ready(function(){
 
     $(document).on("click", "button", function(){
         number = Number($("input").val());
-        checkNumber(2);
-        checkNumber(3);
-        while(number > 1){
-            primeList.push(newPrime());
-            checkNumber(primeList[primeList.length - 1]);
-        }
-        var content = "",
-        check = answer.length - 2;
-        answer.forEach(function(item, i){
-            content += item.toLocaleString();
-            if(i <= check){
-                content += " * ";
+        if(Number.isSafeInteger(number)){
+            checkNumber(2);
+            checkNumber(3);
+            while(number > 1){
+                primeList.push(newPrime());
+                checkNumber(primeList[primeList.length - 1]);
             }
-        });
-        $("div").html(content);
-        primeList.splice(1, primeList.length - 1);
-        answer.splice(0, answer.length);
-        count = 0;
-        notPrime = 9;
+            var content = "",
+            check = answer.length - 2;
+            answer.forEach(function(item, i){
+                content += item.toLocaleString();
+                if(i <= check){
+                    content += " * ";
+                }
+            });
+            $("div").html(content);
+            primeList.splice(1, primeList.length - 1);
+            answer.splice(0, answer.length);
+            count = 0;
+            notPrime = 9; 
+        } else {
+            alert("The input is to high to calculate with!");
+        }
     });
 
     function checkNumber(item){
@@ -48,7 +52,9 @@ $(document).ready(function(){
     function eratosthenes(x){
         if(x == notPrime){
             count++;
-            notPrime = primeList[count] * primeList[count];
+            if(Number.isSafeInteger(primeList[count] * primeList[count])){
+                notPrime = primeList[count] * primeList[count];
+            }
             return false;
         }
         for(i = 0; i < count; i++){
