c39260a18d072c1c35c6cd55aa11fc34df608cbb
Damorn Tyde <34279265+DamornTyde@users.noreply.github.com>
2021-02-10 14:40:31 +0100

    Update

--- functions.js
+++ functions.js
@@ -1,63 +1,66 @@
-$(document).ready(function(){
-    var primeList = [3],
-    answer = [],
-    number;
+var primes;
 
-    $(document).on("click", "button", function(){
-        number = Number($("input").val());
-        if(Number.isSafeInteger(number)){
-            var content = number.toLocaleString() + " = ";
-            checkNumber(2);
-            checkNumber(3);
-            while(number > 1){
-                primeList.push(newPrime());
-                checkNumber(primeList[primeList.length - 1]);
+document.getElementById("submit").addEventListener("click", function () {
+    const input = Number(document.getElementById("input").value);
+    if (Number.isSafeInteger(input) && input > 1) {
+        const answer = calc(input);
+        const output = document.createElement("p");
+        output.appendChild(document.createTextNode(`${input} = `));
+        var i = 0;
+        while (true) {
+            const x = answer[i];
+            output.appendChild(document.createTextNode(x));
+            const y = answer.filter(z => z == x).length;
+            if (y > 1) {
+                const sup = document.createElement("sup");
+                sup.appendChild(document.createTextNode(y));
+                output.appendChild(sup);
             }
-            var check = answer.length - 2;
-            answer.forEach(function(item, i){
-                content += item.toLocaleString();
-                if(i <= check){
-                    content += " * ";
-                }
-            });
-            $("div").html(content);
-            primeList.splice(1, primeList.length - 1);
-            answer.splice(0, answer.length); 
-        } else {
-            alert("The input is to high to calculate with!");
-        }
-    });
-
-    function checkNumber(item){
-        if(number > 1 && item * item > number){
-            answer.push(number);
-            number = 1;
-        } else {
-            var check = number / item;
-            if(Number.isInteger(check)){
-                number = check;
-                answer.push(item);
-                checkNumber(item);
+            i += y;
+            if (i == answer.length) {
+                const print = document.getElementById("print");
+                print.innerHTML = "";
+                print.appendChild(output);
+                return;
+            } else {
+                output.appendChild(document.createTextNode(" * "));
             }
         }
-    }
-
-    function newPrime(){
-        for(x = primeList[primeList.length - 1] + 2;;x += 2){
-            if(eratosthenes(x)){
-                return x;
-            }
-        }
-    }
-
-    function eratosthenes(x){
-        for(i = 0;;i++){
-            if(Math.pow(primeList[i], 2) > x){
-                return true;
-            }
-            if(Number.isInteger(x / primeList[i])){
-                return false;
-            }
-        }
+    } else {
+        alert("Invalid input!");
     }
 });
+
+function calc(input) {
+    const answer = [];
+    primes = [2];
+    while (true) {
+        if (Math.pow(primes[primes.length - 1], 2) > input) {
+            answer.push(input);
+            return answer;
+        } else if (input % primes[primes.length - 1] == 0) {
+            answer.push(primes[primes.length - 1]);
+            input = input / primes[primes.length - 1];
+        } else {
+            primes.push(newPrime());
+        }
+    }
+}
+
+function newPrime() {
+    for (var x = primes[primes.length - 1] + 1;; x++) {
+        if (eratosthenes(x)) {
+            return x;
+        }
+    }
+}
+
+function eratosthenes(x) {
+    for (var i = 0;; i++) {
+        if(Math.pow(primes[i], 2) > x) {
+            return true;
+        } else if (x % primes[i] == 0) {
+            return false;
+        }
+    }
+}
